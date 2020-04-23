6efda42f7852e7b4ff69f812fb683bc13d91adb1
Joshua Deare <me@joshdea.re>
2020-04-23 14:44:02 -0700

    try and fix repl

--- cmd/server.go
+++ cmd/server.go
@@ -1,19 +1,18 @@
 package main
 
 import (
-  "github.com/doodles526/replCodeExec/server"
-  "github.com/doodles525/replCodeExec/executor"
+	"github.com/doodles526/replCodeExec/execution"
+	"github.com/doodles526/replCodeExec/server"
 )
 
 func main() {
-  eArgs := executor.PoolArgs {
-  }
-  executor := executor.Executor(eArgs)
+	eArgs := executor.PoolArgs{}
+	executor := executor.Executor(eArgs)
 
-  args := server.Args {
-    ListenAddr: "localhost:8080",
-    Executor: executor,
-  }
+	args := server.Args{
+		ListenAddr: "localhost:8080",
+		Executor:   executor,
+	}
 
-  server.ServerCodeExecution(&args)
+	server.ServerCodeExecution(&args)
 }
--- messages/serverToClient.go
+++ messages/serverToClient.go
@@ -1,25 +1,25 @@
 package messages
 
 const (
-  CodeResponseGraphicalType = "resp-graphical" 
-  CodeResponseTerminalType = "resp-terminal" 
-  CodeResponseError = "resp-error"
+	CodeResponseGraphicalType = "resp-graphical"
+	CodeResponseTerminalType  = "resp-terminal"
+	CodeResponseErrorType     = "resp-error"
 )
 
 // GraphicalCodeResponse should result in a ref to an image
 // To render on screen
 type GraphicalCodeResponse struct {
-  Reference string `json:"ref"`
-  Location string `json:"location"`
-  SessionID *string `json:"session_id,omitempty"`
+	Reference string  `json:"ref"`
+	Location  string  `json:"location"`
+	SessionID *string `json:"session_id,omitempty"`
 }
 
 type TerminalCodeResponse struct {
-  Text string `json:"text"`
-  ReservedExpansionReferences map[string]string `json:"reserved_refs"`
-  SessionID *string `json:"session_id,omitempty"`
+	Text                        string            `json:"text"`
+	ReservedExpansionReferences map[string]string `json:"reserved_refs"`
+	SessionID                   *string           `json:"session_id,omitempty"`
 }
 
 type CodeResponseError struct {
-  Error string `json:"error"`
+	Error string `json:"error"`
 }
